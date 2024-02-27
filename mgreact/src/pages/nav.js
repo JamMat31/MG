af0d65d00902a0331090963de9ff13e0645574a3
James <jamesbusiness3005@gmail.com>
2024-02-27 15:56:01 -0500

    <nessage>

---
+++ mgreact/src/pages/nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <a  href="#" className="logo">Logo</a>
+            <ul className="nav-links">
+                <li><a href="/">Home</a> </li>
+                <li><a href="/about"></a></li>
+
+            </ul>
+        </div>
+    );
+};
+export default Navbar;
