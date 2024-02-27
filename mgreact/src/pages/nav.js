474d276cc2796f0b7af84c6357af8aa7ba591eab
James <jamesbusiness3005@gmail.com>
2024-02-27 18:34:32 -0500

    created navbar with no link setup

--- mgreact/src/pages/nav.js
+++ mgreact/src/pages/nav.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import './nav.css'
+import Logo from './images/CT_logo.png'
 const Navbar = () => {
     return (
         <div className="navbar">
-            <a  href="#" className="logo">Logo</a>
+            <a  href="#" className="logo"><img src={Logo} alt='logo' style={{width:'200px'}}></img></a>
             <ul className="nav-links">
-                <li><a href="/">Home</a> </li>
+                <li><a href="/">About Us</a> </li>
+                <li><a href="/">Equipment</a> </li>
+                <li><a href="/">Permit Classes</a> </li>
                 <li><a href="/about"></a></li>
 
             </ul>
         </div>
     );
 };
-export default Navbar;
+export default Navbar; 
